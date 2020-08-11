f4847e3003474a8fb97176c821966a4c04f84758
balaji <dineshbalajipm@gmail.com>
2020-08-11 21:01:24 +0530

    balaji's first add program with unittesting commit

---
+++ test/testaddition.py
@@ -0,0 +1,17 @@
+import unittest
+
+from src.addition import add
+
+
+class AdditionTest(unittest.TestCase):
+    def testadd(self):
+        result=add(1,1)
+        self.assertEqual(2,result)
+
+    def teststrinadd(self):
+        result = add("bal","aji")
+        self.assertEqual("balaji", result)
+
+    def testaddnegativenumbers(self):
+        result=add(-1,-1)
+        self.assertEqual(-2,result)
